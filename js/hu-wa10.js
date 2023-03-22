f18d80eef6d3bff501d105b0d59c82f0032427da
hailey <haileyusher@sonic.net>
2023-03-22 09:48:18 -0600

    dfsdfsdfsf

--- js/hu-wa10.js
+++ js/hu-wa10.js
@@ -1,18 +1,39 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+/* img loop access */
+  function thumbnailLoop() {
+  var i;
+  for (i = 0; i < 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', "/img/pic" + (i+1) + ".jpg");
+  thumbBar.appendChild(newImage);
+}
+}
 
-/* Declaring the alternative text for each image file */
+thumbnailLoop();
 
-/* Looping through images */
+/* click thumbnail --> main img */
+thumbBar.addEventListener('click', (e) => {
+  if(e.target && e.target.nodeName == "IMG") {
+    displayedImage.src = e.target.src;
+}
+}); 
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+/* darken/lighten button */
+btn.addEventListener('click', (e) => {
+  if (e.target.className == 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
 
-/* Wiring up the Darken/Lighten button */
+ else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+}
+});
