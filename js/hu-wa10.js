fd698a3c8319d54084dfe121c2fa2552763c3f6d
hailey <haileyusher@sonic.net>
2023-03-23 12:52:52 -0600

    sdfsdfsdfsfdsdf

--- js/hu-wa10.js
+++ js/hu-wa10.js
@@ -4,12 +4,15 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
+const alts = ['A mountain surrounded by fog.','a green and pink spiral','a very cute bunny','an ocean wave.','a field of California Poppies.'];
+
 /* img loop access */
   function thumbnailLoop() {
   var i;
   for (i = 0; i < 5; i++) {
   var newImage = document.createElement('img');
   newImage.setAttribute('src', "/img/pic" + (i+1) + ".jpg");
+  newImage.setAttribute('alt',alts[i]);
   thumbBar.appendChild(newImage);
 }
 }
@@ -20,6 +23,7 @@ thumbnailLoop();
 thumbBar.addEventListener('click', (e) => {
   if(e.target && e.target.nodeName == "IMG") {
     displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
 }
 }); 
 
