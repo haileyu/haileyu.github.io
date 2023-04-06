15d25644bfb151a509b4fea3c4dabab82a8c6c2b
hailey <haileyusher@sonic.net>
2023-04-06 13:39:15 -0600

    sadasd

--- js/hu-wa11.js
+++ js/hu-wa11.js
@@ -3,18 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const quote = document.querySelector("#js-quote-text");
     const cite = document.querySelector("#js-quote-author");
 
- async function getQuote() {
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    if (response.ok) {
-        quote.textContent = data.content;
-        cite.textContent = data.author;
-    }else{
-        quote.textContent = "failed to fetch quote :(";
-        console.log(data);
+    async function getQuote() {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        if (response.ok) {
+            quote.textContent = data.content;
+            cite.textContent = data.author;
+        }else{
+            quote.textContent = "failed to fetch quote :(";
+            console.log(data);
+        }
     }
-}
     button.addEventListener("click", getQuote);
+    getQuote();
 });
 
 const twitterURL = "https://twitter.com/intent/tweet/";
@@ -22,23 +23,26 @@ const linkTarget = '_top';
 const windowOptions = 'menubar=no,status=no,height=750,width=500';
 
 function extractQuote(){
-    return document.querySelector('#js-quote-text');
+    console.log(document.querySelector('#js-quote-text').textContent);
+    return document.querySelector('#js-quote-text').textContent;
+    
 }
 
 function extractAuthor(){
-    return document.querySelector('#js-quote-author');
+    console.log(document.querySelector('#js-quote-author').textContent);
+    return document.querySelector('#js-quote-author').textContent;
 }
 
-function openTwitterWindow(text, link) {
-    const twitterQuery = `text=${text}&url=${link}`;
-    return window.open(`${twitterUrl}?${twitterQuery}&`, linkTarget, windowOptions);
+function openTwitterWindow() {
+    const quote = extractQuote();
+    const author = extractAuthor();
+    const twitterQuery = `text=${encodeURIComponent(`"${quote}" - ${author}`)}&url=`;
+    return window.open(`${twitterURL}?${twitterQuery}&`, linkTarget, windowOptions);
 }
 
 function shareQuote() {
-    const quote = extractQuote()
-    const author = extractAuthor();
     const twitterButton = document.querySelector('#js-tweet-button');
-    twitterButton.addEventListener('click', () => openTwitterWindow(quote, author))
+    twitterButton.addEventListener('click', () => openTwitterWindow());
 }
 
-shareQuote()
+shareQuote();
